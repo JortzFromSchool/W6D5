9d89375ab80c9cef752ddce2eaec223bf5ef6fd8
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-09-17 17:23:25 -0400

    ended on phase 3

--- 99_cats/cats/app/controllers/cat_rental_requests_controller.rb
+++ 99_cats/cats/app/controllers/cat_rental_requests_controller.rb
@@ -3,9 +3,30 @@ class CatRentalRequestsController < ApplicationController
     def show
         @cat_rental = CatRentalRequest.find(params[:id])
         render :show
+    end
+
+    def new 
+        @cat_rental = CatRentalRequest.new
+        render :new
+    end
+
+    def create 
+        @cat_rental = CatRentalRequest.new(rental_params)
+        if @cat_rental.save
+            redirect_to cat_rental_request_url(@cat_rental)
+        else
+            render json: @cat_rental.errors.full_messages, status: 422
+        end
+
 
     end
 
+
+    private
+    def rental_params
+        params.require(:cat_rental).permit(:cat_id, :start_date, :end_date)
+
+    end
 
 
 
--- 99_cats/cats/config/application.rb
+++ 99_cats/cats/config/application.rb
@@ -17,3 +17,15 @@ module Cats
     # the framework and any gems in your application.
   end
 end
+
+module CatRentalRequests
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.action_controller.default_protect_from_forgery = false
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
