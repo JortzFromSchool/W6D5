2aa0949bc2444863a2eeeb8e558f6d39d45f3a11
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-09-17 15:02:56 -0400

    finished working on new

--- 99_cats/cats/app/controllers/cats_controller.rb
+++ 99_cats/cats/app/controllers/cats_controller.rb
@@ -4,4 +4,31 @@ class CatsController < ApplicationController
         @cats = Cat.all
         render :index
     end
+
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create 
+        @cat = Cat.new(cat_params)
+        if @cat.save 
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
+    end
+
+
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :birthdate, :color, :sex, :description)
+    end
 end
--- 99_cats/cats/db/seeds.rb
+++ 99_cats/cats/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.destroy_all
+tom = Cat.create(birthdate: '2015/01/01', color: 'brown', name: 'tom', sex: 'M', description: 'Tom is the best' )
+mike = Cat.create(birthdate: '2017/01/01', color: 'white', name: 'mike', sex: 'M', description: 'Mike is the best' )
+evan = Cat.create(birthdate: '2014/01/01', color: 'black', name: 'evan', sex: 'M', description: 'Evan is the best' )
+cathy = Cat.create(birthdate: '2013/01/01', color: 'grey', name: 'cathy', sex: 'F', description: 'Cathy is the best' )
+
